75b1da1b9516ce1ee5308c6162831090a32aeeda
ayfor <josh.a.stubbington@gmail.com>
2021-03-29 22:08:08 -0400

    added modal to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready( function () {
     $('#grocList').DataTable();
+    $('.modal').modal();  
 } );
+
+var newItemButtonElement = $("#new-item-button");
+var newItemFormElement = $('#new-item-modal');
+
+    
+
