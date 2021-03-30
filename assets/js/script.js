9ccd05378cc8fd857cc4ae6678f6c27307e10285
ayfor <josh.a.stubbington@gmail.com>
2021-03-29 22:27:17 -0400

    added modal inputs to an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,34 @@ $(document).ready( function () {
     $('.modal').modal();  
 } );
 
+//Array for storing current grocery entries
+var groceryItemArray = [];
+
 var newItemButtonElement = $("#new-item-button");
 var newItemFormElement = $('#new-item-modal');
 
-    
+//Variables for modal 
+var groceryItemInputElement = $('#grocery-item-input');
+var expirationDateInputElement = $('#expiration-date-input');  
+var submitGroceryItemElement = $('#submit-grocery-item'); 
 
+
+function addGroceryItem(event) { 
+    //Create new item to store inputted values
+    let newItem = {
+        "label":'',
+        "expirationDate":''
+    }
+
+    //Add inputs from modal fields
+    newItem.label = groceryItemInputElement.val();
+    newItem.expirationDate = expirationDateInputElement.val();
+
+    //Add new item to array
+    groceryItemArray.push(newItem);
+
+    //Console log to view
+    console.log(newItem);
+}
+
+submitGroceryItemElement.click(addGroceryItem);
